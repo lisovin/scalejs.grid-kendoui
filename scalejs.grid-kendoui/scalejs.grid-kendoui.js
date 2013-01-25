dcd65b9040169a084c6b6bc6f2c1edc20faa3f3f
Peter Lisovin <peter@lisovin.com>
2013-01-25 14:11:33 -0500

    Improved kendo nuget libs.

--- scalejs.grid-kendoui/scalejs.grid-kendoui.js
+++ scalejs.grid-kendoui/scalejs.grid-kendoui.js
@@ -1,40 +1,19 @@
 /*global define*/
 define([
-    './scalejs.grid-kendoui/part1'
+    'scalejs.grid-kendoui/grid',
+    'jQuery',
+    'knockout',
+    'knockout.mapping'
 ], function (
-    part1
+    kendoGrid,
+    $,
+    ko
 ) {
     'use strict';
 
-    // Alternative method to extend the core would be to have 
-    // buildCore function that extends the core with functionality
-    /*
-    function buildCore(core) {
-        var extend = core.object.extend;
+    $('head').prepend('<link rel="stylesheet" type="text/css" href="css/kendo.common.min.css"></link>');
+    $('head').prepend('<link rel="stylesheet" type="text/css" href="css/kendo.default.min.css"></link>');
 
-        extend(core, { part1 : part1 });
-    }
-    */
-    // Alternative method to extend the sandbox would be to have 
-    // buildSandbox function that extends the sandbox with functionality
-    /*
-    function buildSandbox(sandbox) {
-        var extend = core.object.extend;
-
-        extend(sandbox, { part1 : part1 });
-    }
-    */
-    return {
-        // Set the object that would extend the core.
-        // Alternatively you can provide buildCore function.
-        core: {
-            part1 : part1
-        },
-        // Set the object that would extend the sandbox.
-        // Alternatively you can provide buildSandbox function.
-        sandbox: {
-            part1 : part1
-        }
-    };
+    ko.bindingHandlers.kendoGrid = kendoGrid;
 });
 
--- scalejs.grid-kendoui/tools/install.ps1
+++ scalejs.grid-kendoui/tools/install.ps1
@@ -3,15 +3,9 @@
 $project |
 	Add-Paths "{
 		'scalejs.grid-kendoui' : 'Scripts/scalejs.grid-kendoui-$($package.Version)',
-		'jQuery' : 'Scripts/jquery-1.9.0',
 		'kendo.grid' : 'Scripts/kendo.grid.min',
 		'knockout' : 'Scripts/knockout-2.2.1',
 		'knockout.mapping' : 'Scripts/knockout.mapping-latest'
 	}" |
-	Add-Shims "{ 
-		'jQuery' : {
-			'exports' : 'jQuery'
-		}
-	}" | 
 	Add-ScalejsExtension 'scalejs.grid-kendoui' |
 	Out-Null
